019e3efc07cf1d03dc8577dd0a3b69446661cf42
Neetesh Prajapati <139002314+Neetesh26@users.noreply.github.com>
2025-06-29 00:01:57 +0530

    usermodel

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: [3, 'Username must be at least 3 characters long']
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: [13, 'Email must be at least 5 characters long']
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [6, 'Password must be at least 6 characters long']
+    }
+})
